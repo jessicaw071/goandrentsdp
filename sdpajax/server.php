5c16da96a2f8afcb68f5f827bae3e572471cf939
timotirta <timotius.tirtawan@gmail.com>
2017-12-20 15:53:55 +0700

    commit2

--- sdpajax/server.php
+++ sdpajax/server.php
@@ -24,19 +24,19 @@
 		// Create connection
 		$link = new mysqli($servername, $user, $pass, $dbname);
 		if($_POST['tipe'] == 'ajaxLogin') {
-			$query = "SELECT * FROM users";
+			$query = "SELECT * FROM userproyeks";
 			$res = $link->query($query);
 			$data = array();
 			while($row = $res->fetch_object()){
               $data[] = $row;
           	}
 			echo json_encode($data);
-		}else if($_POST['tipe'] == 'isiinSession') {
-			$_SESSION['userskr'] = $_POST['user'];
-			echo $_SESSION['userskr'];
 		}else if($_POST['tipe'] == 'updateLongLat') {
 			$longlat = $_POST['lat'].' '.$_POST['long'];
 			$query = "UPDATE rumahs SET longlat_rumah = '".$longlat."' WHERE id_rumah = '".$_POST['id']."'";
+			$link->query($query);
+		}else if($_POST['tipe'] == 'updateTitleRumah') {
+			$query = "UPDATE rumahs SET title_rumah = '".$_POST['title']."' WHERE id_rumah = '".$_POST['id']."'";
 			$link->query($query);
 		}else if($_POST['tipe'] == 'ambilSemuaRumah') {
 			$query = "SELECT * FROM rumahs";
@@ -56,20 +56,28 @@
 				$data[] = $row;
     	}
 			echo json_encode($data);
-		}else if($_POST['tipe'] == 'tampilinSession') {
-			if(! isset($_SESSION['userskr'])) $_SESSION['userskr'] = "tidak ada";
-			echo $_SESSION['userskr'];
+		}else if($_POST['tipe'] == 'ambilHighRating') {
+			$query = "SELECT * FROM rumahs ORDER BY rating DESC";
+			$res = $link->query($query);
+			$data = array();
+			while($row = $res->fetch_object()){
+				$pecah = explode(" ",$row->longlat_rumah);
+				$row->distance = distance($_POST['longkita'],$_POST['latkita'],$pecah[0],$pecah[1],"K");
+				$data[] = $row;
+    	}
+			echo json_encode($data);
 		}else if($_POST['tipe'] == 'mintamd5php') {
 			echo md5($_POST['pass']);
 		}else if($_POST['tipe'] == 'ajaxRegister') {
 			// tipe 2 kirim chat
-			$query = "SELECT * FROM users";
+			$query = "SELECT * FROM userproyeks";
 			$ketemu = false;
 			$res = $link->query($query);
 			while($row = $res->fetch_object()){
-				if($_POST['user'] == $row->email){
+				if($_POST['user'] == $row->email_user){
 					if($_POST['pass'] == 'google'){
-
+						$ketemu=true;
+						break;
 					}else{
 						$ketemu = true;
 						break;
@@ -77,12 +85,16 @@
 				}
           	}
           	if($ketemu){
-          		echo "Username sudah terdaftar!";
+          		if($_POST['pass'] != 'google')echo "Username sudah terdaftar!"; else echo "b";
           	}else{
-          		$query = "INSERT INTO `users`(`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES (NULL,'".$_POST['nama']."','".$_POST['user']."','".md5($_POST['pass'])."',null,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)";
-				$link->query($query);
-				$yangdiecho = $_POST['pass'] != 'google' ? "a" : "b";
-				echo $yangdiecho;
+          		if($_POST['pass'] != 'google'){
+								$query = "INSERT INTO `userproyeks`(`id_user`, `pw_user`, `nama_user`, `email_user`, `jk_user`, `telp_user`, `tgl_lahir_user`, `alamat_user`, `status_user`, `picture_user`, `created_at`, `updated_at`) VALUES ('".$_POST['user']."','".md5($_POST['pass'])."','".$_POST['nama']."','".$_POST['email']."','".$_POST['gender']."','".$_POST['telp']."','".$_POST['dob']."','".$_POST['alamat']."','aktif','https://www.thesourcepartnership.com/wp-content/uploads/2017/05/facebook-default-no-profile-pic-300x300.jpg',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)";
+							}else {
+								$query = "INSERT INTO `userproyeks`(`id_user`, `pw_user`, `nama_user`, `email_user`, `status_user`, `picture_user`, `created_at`, `updated_at`) VALUES ('".$_POST['user']."','".md5($_POST['pass'])."','".$_POST['nama']."','".$_POST['email']."','aktif','https://www.thesourcepartnership.com/wp-content/uploads/2017/05/facebook-default-no-profile-pic-300x300.jpg',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)";
+							}
+							$link->query($query);
+							$yangdiecho = $_POST['pass'] != 'google' ? "a" : "b";
+							echo $yangdiecho;
           	}
 		}
         /*elseif($_POST['tipe'] == '1') {
